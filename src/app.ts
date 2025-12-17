c072994c9acd6ad796b03c26fc544c8228e76139
Divit Periwal <divitperiwal@gmail.com>
2025-12-17 17:44:57 +0530

    feat: add required packages and create health routes

---
+++ src/app.ts
@@ -0,0 +1,33 @@
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import helmet from "helmet";
+import { cookieOptions, corsOptions } from "@/constant.js";
+import authRoutes from "@/routes/auth.routes.js";
+import { sendSuccess } from "./utils/response.js";
+import { errorHandler, notFound } from "./middlewares/error.middleware.js";
+const app = express();
+app.disable("x-powered-by");
+
+app.use(helmet());
+app.use(cors(corsOptions));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+//Health Routes
+app.get("/", (req, res) => {
+  sendSuccess(res, 200, "Welcome to the API");
+});
+
+app.get("/health", (req, res) => {
+  sendSuccess(res, 200, "Server is healthy", { timestamp: Date.now() });
+});
+
+//Routes
+app.use("/api/v1/auth", authRoutes);
+
+app.use(errorHandler);
+app.use(notFound);
+
+export default app;
--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,10 @@
-import express from "express";
+import dotenv from "dotenv";
+import app from "./app.js";
 
-const app = express();
+dotenv.config();
 
 const PORT = process.env.PORT || 8000;
 
-app.get("/", (req, res) => {
-  res.send("Hello, World!");
-});
-
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
