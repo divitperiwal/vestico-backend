0b8a9d315f2f726fbb41793dc8b6fffebbe8f93e
Divit Periwal <divitperiwal@gmail.com>
2025-12-17 17:46:27 +0530

    refactor(error): add apierror to throw error

---
+++ src/utils/ApiError.ts
@@ -0,0 +1,32 @@
+import type { ApiErrorResponse } from "@/types/common.js";
+
+export class ApiError extends Error {
+  public statusCode: number;
+  public status: any;
+  public details: any;
+
+  constructor(message: string, statusCode: number, status?: any, details?: any) {
+    super(message);
+    this.name = this.constructor.name;
+
+    this.statusCode = statusCode;
+    this.status = status;
+    this.details = details;
+
+    Error.captureStackTrace(this, this.constructor);
+  }
+
+  toJSON() {
+    const response: ApiErrorResponse = {
+      success: false,
+      statusCode: this.status,
+      message: this.message,
+      code: this.statusCode,
+    };
+
+    if (process.env.NODE_ENV === "development" && this.details) {
+      response.details = this.details;
+    }
+    return response;
+  }
+}
