08b6c795940a4ed7208e0c95802353b3b2edb55c
Divit Periwal <divitperiwal@gmail.com>
2025-12-19 23:55:47 +0530

    chore(db): add drizzle orm

---
+++ src/config/drizzle.config.ts
@@ -0,0 +1,9 @@
+import { neon } from "@neondatabase/serverless";
+import dotenv from "dotenv";
+import { drizzle } from "drizzle-orm/neon-http";
+import * as schema from "@/database/schema/index.js";
+
+dotenv.config();
+
+const sql = neon(process.env.DATABASE_URL as string);
+export const db = drizzle(sql, { schema });
