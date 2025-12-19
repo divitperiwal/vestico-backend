b563ac0c0bcc9735a8cf7e98737799dee0c15fa3
Divit Periwal <divitperiwal@gmail.com>
2025-12-20 01:26:32 +0530

    refactor(middleware): add handling of zod error

--- src/middlewares/error.middleware.ts
+++ src/middlewares/error.middleware.ts
@@ -1,9 +1,12 @@
 import {sendError} from "@/utils/response.js";
 import type { NextFunction, Request, Response } from "express";
+import { ZodError } from "zod";
 
 export const errorHandler = (error:Error, req:Request, res:Response, next:NextFunction) => {
     if(error.name === "ValidationError") return sendError(res, 400, "Validation Error", error);
     if (error.name === "UnauthorizedError") return sendError(res, 401, "Unauthorized Access", error);
+    
+    if(error instanceof ZodError) return sendError(res, 400, error.issues[0].message || "Validation Error", error.issues);
     
     return sendError(res, 500, error.message || "Internal Server Error", process.env.NODE_ENV === "development" ? error.stack : undefined);
     
