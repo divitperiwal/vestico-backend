829efde27701a982cb8a05ba7c7379a53331635d
Divit Periwal <divitperiwal@gmail.com>
2025-12-19 20:06:17 +0530

    security: add csrf token generation

--- src/utils/cookies.ts
+++ src/utils/cookies.ts
@@ -1,3 +1,6 @@
+import { CSRF_COOKIE_NAME } from "@/constant.js";
+
+//Session Cookie Utilities
 export const createSessionCookie = (sessionId: string) => {
   const secure = process.env.NODE_ENV === "production";
   return `session=${sessionId}; HttpOnly; Path=/; SameSite=Lax; ${secure ? "Secure;" : ""}`;
@@ -9,7 +12,25 @@ export const clearSessionCookie = () => {
 
 export const readSessionCookie = (cookieHeader?: string) : string | null => {
     if (!cookieHeader) return null;
-    const cookies = Object.fromEntries(cookieHeader.split(';').map((c) => c.split('=')));
+    const cookies = Object.fromEntries(cookieHeader.split(';').map((c) => c.trim().split('=')));
 
     return cookies['session'] || null;
+}
+
+
+//CSFR Token Cookie Utilities
+export const createCsrfCookie = (token: string) => {
+  const secure = process.env.NODE_ENV === "production";
+  return `${CSRF_COOKIE_NAME}=${token}; Path=/; SameSite=Lax ; ${secure ? "Secure;" : ""}`;
+}
+
+export const readCsrfCookie = (cookieHeader?:string) : string | null => {
+  if(!cookieHeader) return null;
+  const cookies = Object.fromEntries(cookieHeader.split(';').map((c) => c.split('=')));
+
+  return cookies[CSRF_COOKIE_NAME] || null;
+}
+
+export const clearCsrfCookie = () => {
+  return `${CSRF_COOKIE_NAME}=; Path=/; Max-Age=0; SameSite=Lax ;`;
 }
---
+++ src/utils/tokenGeneration.ts
@@ -0,0 +1,9 @@
+import crypto from "crypto";
+
+export const generateSessionId = (): string => {
+  return crypto.randomBytes(32).toString("hex");
+};
+
+export const generateCsrfToken = (): string => {
+  return crypto.randomBytes(32).toString("hex");
+};
