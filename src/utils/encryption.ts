5af75bfc68c7126572b51ed18f2e90fab7e03368
Divit Periwal <divitperiwal@gmail.com>
2025-12-21 01:22:35 +0530

    security: add encryption for storing credentials in db

---
+++ src/utils/encryption.ts
@@ -0,0 +1,28 @@
+import crypto from "crypto";
+
+const ENCRYPTION_KEY = Buffer.from(process.env.ENCRYPTION_KEY as string, 'hex');
+const IV_LENGTH = 16; 
+
+export const encryptData = (data: string) => {
+    const iv = crypto.randomBytes(IV_LENGTH);
+    const cipher = crypto.createCipheriv('aes-256-gcm',ENCRYPTION_KEY, iv);
+    const encrypted = Buffer.concat([cipher.update(data, 'utf8'), cipher.final()]);
+
+    const authTag = cipher.getAuthTag();
+    
+    return [iv.toString('hex'), authTag.toString('hex'), encrypted.toString('hex')].join(':');
+}
+
+export const decryptData = (encryptedData: string) => {
+    const [ivHex, authTagHex, encryptedHex] = encryptedData.split(':');
+    const iv = Buffer.from(ivHex, 'hex');
+    const authTag = Buffer.from(authTagHex, 'hex');
+    const encryptedText = Buffer.from(encryptedHex, 'hex');
+
+    const decipher = crypto.createDecipheriv('aes-256-gcm', ENCRYPTION_KEY, iv);
+    
+    decipher.setAuthTag(authTag);
+
+    const decrypted = Buffer.concat([decipher.update(encryptedText), decipher.final()]);
+    return decrypted.toString('utf8');
+}
