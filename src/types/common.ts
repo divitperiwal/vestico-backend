8a2d3485834a708f05481ff58288b403de4244f9
Divit Periwal <divitperiwal@gmail.com>
2025-12-21 01:22:14 +0530

    security: add admin validatons and typescript types

--- src/types/common.ts
+++ src/types/common.ts
@@ -19,3 +19,18 @@ export interface ApiErrorResponse {
   code: number;
   details?: any;
 }
+
+export interface BaseBrokerCredentials {
+  accessToken?: string;
+  accessTokenExpiry?: string;
+} 
+
+export interface DhanCredentials extends BaseBrokerCredentials {
+  clientId: string;
+  apiKey: string;
+  apiSecret: string;
+}
+export interface MstockCredentials extends BaseBrokerCredentials {
+  apiKey: string;
+  totpSecret: string;
+}
--- src/types/validation/admin.validation.ts
+++ src/types/validation/admin.validation.ts
@@ -1,5 +1,34 @@
-import {z} from "zod";
+import { z } from "zod";
+import { strategyEnum, brokerEnum } from "@/database/schema/enums.schema.js";
 
-export const GetUserParamsSchema = z.object({
+export const UserParamsSchema = z
+  .object({
     id: z.uuid("Invalid user ID").trim(),
+  })
+  .strict();
+
+export const UpdateUserParamsSchema = z
+  .object({
+    name: z
+      .string()
+      .min(1, "Name cannot be empty")
+      .max(50, "Name is too long")
+      .trim()
+      .optional(),
+    email: z.email("Invalid email address").trim().optional(),
+    strategy: z.enum(strategyEnum.enumValues, "Invalid strategy").optional(),
+  })
+  .strict();
+
+export const DhanCredentialsSchema = z.object({
+  clientId: z.string().min(1, "Client ID cannot be empty").trim().optional(),
+  apiKey: z.string().min(1, "API Key cannot be empty").trim().optional(),
+  secretKey: z.string().min(1, "Secret Key cannot be empty").trim().optional(),
 }).strict();
+
+export const MstockCredentialsSchema = z.object({
+  apiKey: z.string().min(1, "API Key cannot be empty").trim().optional(),
+  totpKey: z.string().min(1, "TOTP Key cannot be empty").trim().optional(),
+}).strict();
+
+
