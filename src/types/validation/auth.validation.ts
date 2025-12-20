eef5ab04808846a45469238561911f63279d9d37
Divit Periwal <divitperiwal@gmail.com>
2025-12-21 01:23:03 +0530

    refactor: add more precise validation

--- src/types/validation/auth.validation.ts
+++ src/types/validation/auth.validation.ts
@@ -7,6 +7,6 @@ export const LoginUserSchema = z.object({
 
 export const RegisterUserSchema = z.object({
     email: z.email("Invalid email address").trim().toLowerCase(),
-    password: z.string().min(6, "Password must be at least 6 characters long"),
+    password: z.string().min(6, "Password must be at least 6 characters long").max(50, "Password is too long").trim(),
     name: z.string().min(1, "Name is required").trim().max(50, "Name is too long"),
 }).strict();
