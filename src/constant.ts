18ea692c3e2218b13c3228be89fb500fc5af4292
divitperiwal <divitperiwal@gmail.com>
2026-03-23 19:46:06 +0530

    chore: add default user id for connections

--- src/constant.ts
+++ src/constant.ts
@@ -16,3 +16,5 @@ export const cookieOptions = {
 export const SESSION_DURATION_MS = 12 * 60 * 60 * 1000;
 export const CSRF_COOKIE_NAME = 'csrf_token';
 export const CSRF_HEADER_NAME = 'x-csrf-token';
+
+export const DEFAULT_USERID = '9cda69d0-3387-4e11-a162-5c0082d19b3c';
