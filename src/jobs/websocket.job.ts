145bde79cfd30339e52f19ff3fc519f633fa4f65
divitperiwal <divitperiwal@gmail.com>
2026-03-23 19:47:48 +0530

    feat: implement WebSocket connection management with scheduling and market open checks

---
+++ src/jobs/websocket.job.ts
@@ -0,0 +1,40 @@
+import { DEFAULT_USERID } from "@/constant.js";
+import { MstockService } from "@/modules/broker/mstock/mstock.service.js";
+import { connectMstock, getMstockSocket } from "@/modules/broker/mstock/mstock.ws.js";
+import cron from "node-cron";
+
+
+
+
+export const connectMstockJob = () => {
+    // Schedule to run every day at 9:15 AM
+    cron.schedule('14 9 * * 1-5', () => connectMstockWebSocket(true));
+    cron.schedule('31 15 * * 1-5', () => disconnectMstockWebSocket());
+}
+
+export const connectMstockWebSocket = async (cron = false) => {
+    if (cron) {
+        const { accessToken, apiKey } = await MstockService.getAccessToken(DEFAULT_USERID);
+        connectMstock(apiKey, accessToken);
+    }
+    if (isMarketOpen() && !cron) {
+        const { accessToken, apiKey } = await MstockService.getAccessToken(DEFAULT_USERID);
+        connectMstock(apiKey, accessToken);
+    }
+}
+
+export const disconnectMstockWebSocket = () => {
+    const ws = getMstockSocket()
+    if (ws) ws.close();
+    return;
+}
+
+export function isMarketOpen() {
+    const now = new Date(new Date().toLocaleString("en-US", { timeZone: "Asia/Kolkata" }));
+    const day = now.getDay();
+    if (day === 0 || day === 6) return false;
+
+    const open = new Date(); open.setHours(9, 14, 0, 0);
+    const close = new Date(); close.setHours(15, 30, 0, 0);
+    return now >= open && now <= close;
+}
